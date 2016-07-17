b0da697d4b4e6284c57de42cf9f5b8fe0dac06c9
GTNick <nickpp1111@yahoo.com>
2016-07-17 02:49:25 -0400

    Update Main.php

--- src/aliuly/hud/Main.php
+++ src/aliuly/hud/Main.php
@@ -154,7 +154,7 @@ class Main extends PluginBase implements Listener,CommandExecutor {
 		$fP = $this->getServer()->getPluginManager()->getPlugin("FactionsPro");
                                   $faction = $fP->getPlayerFaction($player->getName());
 		foreach ([
-		                "{faction}" => $this->getFactions($player->getFaction),
+		                "{faction}" => $this->getPlayerFaction($player),
 		                "{power}" => $this->getFactionPower($player->getFaction->getFactionPower));
 		                "{MAXONLINE}" => $this->getServer()->getMaxPlayers();
 				"{bearing}" => self::bearing($player->getYaw()),
